d9d31a7312d6ecafb9727f21df4539e00c3bc135
byosKiTaR10 <os-kita@hotmail.es>
2024-06-08 20:51:12 +0100

    Initial EndPoints

---
+++ WisconsinGacha/Controllers/CardsController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WisconsinGacha.Interfaces;
+using WisconsinGacha.Models;
+
+namespace WisconsinGacha.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CardsController : ControllerBase
+    {
+        private readonly ICardsManager cardsManager;
+
+        public CardsController(ICardsManager cardsManager)
+        {
+            this.cardsManager = cardsManager;
+        }
+
+        [HttpGet]
+        public string GetCards()
+        {
+            List<Card> cards = cardsManager.GetCardsAsync().Result;
+            string result = "{\n";
+            foreach (Card card in cards)
+            {
+                result +=
+                    "    {\n" +
+                    "        'Id': " + card.Id + ",\n"
+                    + "        'Name': " + card.Name + ",\n"
+                    + "        'Image': " + card.Image + ",\n"
+                    + "        'Anime': " + card.Anime + ",\n"
+                    + "        'Rarity': " + card.Rarity + "\n"
+                    + "    },\n";
+            }
+            result = result.Substring(0, result.Length - 2);
+            result += "\n}";
+            return result;
+        }
+
+        [HttpGet("{id}")]
+        public string GetCard(int id) 
+        {
+            string result = "";
+            var card = cardsManager.GetCardAsync(id).Result;
+            if (card == null)
+            {
+                result = "Specified Id does not match any existing card";
+            }
+            else
+            {
+                result =
+                    "{\n" +
+                    "    'Id': " + card.Id + ",\n"
+                    + "    'Name': " + card.Name + ",\n"
+                    + "    'Image': " + card.Image + ",\n"
+                    + "    'Anime': " + card.Anime + ",\n"
+                    + "    'Rarity': " + card.Rarity + "\n}";
+            }
+            
+            return result;
+        }
+
+        [HttpPost]
+        public string CreateCard([FromBody]Card card)
+        {
+            bool cardCreated = cardsManager.CreateCardAsync(card).Result;
+            string result = "";
+            if (cardCreated)
+            {
+                result = "Card was created succesfully.";
+            }
+            else
+            {
+                result = "Card could not be created, check if it already exists.";
+            }
+            return result;
+        }
+
+        [HttpPut]
+        public string UpdateCard([FromBody]Card card)
+        {
+            string result = "";
+            if (cardsManager.UpdateCardAsync(card).Result)
+            {
+                result = "Card has been updated succesfully.";
+
+            } else
+            {
+                result = "Card could not be updated.";
+            }
+            return result;
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteCard(int id) 
+        {
+            bool isCardDeleted = cardsManager.DeleteCardAsync(id).Result;
+            string result = "";
+            if (isCardDeleted)
+            {
+                result = "Card was deleted succesfully";
+            }
+            else
+            {
+                result = "Card could not be deleted";
+            }
+            return result;
+        }
+    }
+}
---
+++ WisconsinGacha/Database/CardsManager.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WisconsinGacha.Interfaces;
+using WisconsinGacha.Models;
+
+namespace WisconsinGacha.Database
+{
+    public class CardsManager : ICardsManager
+    {
+        private readonly AppDbContext context;
+
+        public CardsManager(AppDbContext context)
+        {
+            this.context = context;
+        }
+        
+        public async Task<List<Card>> GetCardsAsync()
+        {
+            return await context.Cards.ToListAsync();
+        }
+
+        public async Task<Card> GetCardAsync(int id) 
+        {
+            var card = await context.Cards.FindAsync(id);
+            return card;
+        }
+
+        public async Task<bool> CreateCardAsync(Card card)
+        {
+            bool isNewCard = false;
+            if (context.Cards.FindAsync(card.Id).Result == null && context.Cards.SingleOrDefaultAsync(x => x.Name == card.Name) == null)
+            {
+                context.Cards.Add(card);
+                await context.SaveChangesAsync();
+                isNewCard = true;
+            }
+            return isNewCard;
+        }
+
+        public async Task<bool> UpdateCardAsync(Card card)
+        {
+            bool isCardUpdated = false;
+            var existingCard = await context.Cards.FindAsync(card.Id);
+            if (existingCard != null)
+            {
+                existingCard.Name = card.Name;
+                existingCard.Image = card.Image;
+                existingCard.Anime = card.Anime;
+                existingCard.Rarity = card.Rarity;
+            }
+            var recordsAffected = await context.SaveChangesAsync();
+            return recordsAffected > 0;
+        }
+
+        public async Task<bool> DeleteCardAsync(int id)
+        {
+            bool isCardDeleted = false;
+
+            var card = await context.Cards.FindAsync(id);
+
+            if (card != null)
+            {
+                context.Cards.Remove(card);
+                await context.SaveChangesAsync();
+                isCardDeleted = true;
+            }
+            return isCardDeleted;
+        }
+
+    }
+}
