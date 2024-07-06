a228fef2943306d65918ed36794d96a8fbf2113f
byosKiTaR10 <os-kita@hotmail.es>
2024-07-06 16:30:55 +0100

    CORS setup

--- WisconsinGacha/Program.cs
+++ WisconsinGacha/Program.cs
@@ -9,10 +9,23 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("WebApiDatabase")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddScoped<ICardsManager, CardsManager>();
 builder.Services.AddScoped<IBannersManager, BannersManager>();
 
+
 var app = builder.Build();
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
